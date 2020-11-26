67ae28a309fc3a448bcf2faf30b0df6e3796c053
samuelfenton <samuel.fenton@outlook.com>
2020-11-26 19:39:31 +1100

    Water shater implemented

---
+++ Assets/Scripts/ShaderFunctions/ShaderFunction_Water.hlsl
@@ -0,0 +1,11 @@
+﻿#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+/// <summary>
+/// Get the required vert data to 
+/// </summary>
+/// <param name="p_elevation">Elevation of node, 0.0f-1.0f</param>
+/// <param name="p_moisture">Moisture of node, 0.0f-1.0f</param>
+/// <returns>Biome type</returns>
+
+#endif
